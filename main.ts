d762e157a8b454f0ffc48968cfa8c60b3e96c0b6
Lorenzo Fiori <127093378+LoryFr@users.noreply.github.com>
2026-07-07 15:14:11 +0200

    update

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { router } from "./app/router.ts";
+
+export default async function handler(request: Request): Promise<Response> {
+  try {
+    return await router.fetch(request);
+  } catch (error) {
+    if (!(request.signal.aborted && error === request.signal.reason)) {
+      console.error(error);
+    }
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
